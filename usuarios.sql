5bcae3bd8bd894955856f39fe76f908b62b4e715
One <projectonecore@gmail.com>
2013-12-17 16:46:29 -0600

    Creando base de datos de usuarios para el panel de administración

---
+++ usuarios.sql
@@ -0,0 +1,35 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Qualtiva
+Source Server Version : 50527
+Source Host           : localhost:3306
+Source Database       : Panel_Administracion
+
+Target Server Type    : MYSQL
+Target Server Version : 50527
+File Encoding         : 65001
+
+Date: 2013-12-17 16:40:44
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `usuarios`
+-- ----------------------------
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `perfil` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- ----------------------------
+-- Records of usuarios
+-- ----------------------------
+INSERT INTO `usuarios` VALUES ('1', 'administrador', 'administrador', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+INSERT INTO `usuarios` VALUES ('2', 'editor', 'editor', 'ab41949825606da179db7c89ddcedcc167b64847');
+INSERT INTO `usuarios` VALUES ('3', 'suscriptor', 'suscriptor', '1a248d7a471ad8d5993aa523c8397ce1d0bafe78');
