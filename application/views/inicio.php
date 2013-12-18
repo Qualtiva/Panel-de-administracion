10ae4252c3077d72bc25173da8f051c64f99c07f
One <projectonecore@gmail.com>
2013-12-18 17:45:29 -0600

    AGREGANDO PAGINA PRINCIPAL

---
+++ application/views/inicio.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+<head>
+	<title>Qualtiva</title>
+	<meta charset="utf8"/>
+<head>
+	<body style="background: black;">
+		<center>
+		<font color="white">
+			<h1>Pagina Principal Qualtiva</h1>
+			<?=anchor('admin/login', lang('admin'));?>
+		</center>
+		</font>
+	</body>
+</html>
